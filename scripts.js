3aaf4150a30c302a0cfe3a3416b784d6ff82574b
Stephanie Lopes <stephanielopees@gmail.com>
2024-09-07 21:09:24 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,144 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+  let url = 'http://127.0.0.1:5000/produtos';
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.produtos.forEach(item => insertList(item.nome, item.defeito, item.status))
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada da função para carregamento inicial dos dados
+  --------------------------------------------------------------------------------------
+*/
+getList()
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (inputProduct, inputDefect, inputStatus) => {
+  const formData = new FormData();
+  formData.append('nome', inputProduct);
+  formData.append('defeito', inputDefect);
+  formData.append('status', inputStatus);
+
+  let url = 'http://127.0.0.1:5000/produto';
+  fetch(url, {
+    method: 'post',
+    body: formData
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  // var table = document.getElementById('myTable');
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+  console.log(item)
+  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo item com nome, quantidade e valor 
+  --------------------------------------------------------------------------------------
+*/
+const newItem = () => {
+  let inputProduct = document.getElementById("newInput").value;
+  let inputDefect = document.getElementById("newDefect").value;
+  let inputStatus = document.getElementById("newStatus").value;
+
+  if (inputProduct === '') {
+    alert("Escreva o nome de um item!");
+  } 
+  else {
+    insertList(inputProduct, inputDefect, inputStatus)
+    postItem(inputProduct, inputDefect, inputStatus)
+    alert("Item adicionado!")
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nameProduct, defect, status) => {
+  var item = [nameProduct, defect, status]
+  var table = document.getElementById('myTable');
+  var row = table.insertRow();
+
+  for (var i = 0; i < item.length; i++) {
+    var cel = row.insertCell(i);
+    cel.textContent = item[i];
+  }
+  insertButton(row.insertCell(-1))
+  document.getElementById("newInput").value = "";
+  document.getElementById("newDefect").value = "";
+  document.getElementById("newStatus").value = "";
+
+  removeElement()
+}
